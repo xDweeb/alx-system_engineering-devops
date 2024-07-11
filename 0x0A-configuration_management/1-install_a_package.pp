8188d055c6e49d143bb5362f92f68494c221a2a5
Taibi EL Yakouti <111664715+xDweeb@users.noreply.github.com>
2024-07-11 19:02:52 +0000

    install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Install flask
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
+# install flask from pip3
+package { 'flask' :
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    }
