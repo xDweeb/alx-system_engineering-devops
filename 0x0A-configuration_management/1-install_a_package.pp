c8cde7a1beae6ac6acbe515f814de4c334e48add
Taibi EL Yakouti <111664715+xDweeb@users.noreply.github.com>
2024-07-11 18:48:02 +0000

    install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,17 @@
-# Install flask with puppet
+#Install flask with puppet
 
 package { 'python3.8':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+  ensure   => '3.0.10',
+  provider => 'pip3',
 }
-package { 'flask':
+
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require => Package['flask'],
 }
