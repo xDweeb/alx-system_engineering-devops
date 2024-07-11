d0f641276df7fa18211fff8163d9c6b7624a1774
Taibi EL Yakouti <111664715+xDweeb@users.noreply.github.com>
2024-07-11 19:27:05 +0000

    install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-#!/usr/bin/pup
-# install a spacific version of flask (2.1.0)
+# A puppet manifest installing flask v2.1.0 which is a package from pip3.
+# Am also specifying the version of flask to install using the ensure attribute
 package { 'flask':
-ensure => '2.1.0',
-provider => 'pip3',
+  ensure   => '2.1.0',
+  provider => pip3,
 }
