2da2e7d1d60f227ef56c1cca497a8d56e2c20311
xdweb <xdweb.elyakouti@gamil.com>
2024-07-11 12:47:29 -0700

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # A puppet manifest installing flask v2.1.0 which is a package from pip3.
-# Am also specifying the version of flask to install using the ensure attribute
-package { 'flask':
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => pip3,
+  provider => 'pip3',
 }
